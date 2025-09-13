a096e066523f37c24f649791635c7f9582c8d896
Allen Jamison <allenjamison027@gmail.com>
2025-09-14 01:58:48 +0800

    Fixed footer spacing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,14 +4,14 @@ import { Copyright } from "lucide-react"
 function Footer() {
   return (
     <section className='border-t-1 border-barline'>
-      <div className='max-w-screen-2xl py-10 px-5 sm:px-8 lg:px-16 mx-auto text-white'>
+      <div className='max-w-screen-2xl py-6 px-5 sm:px-8 lg:px-16 mx-auto text-white'>
         <div className='block md:flex justify-between'>
           <div>
             <NavLink className='text-neutral-300 text-3xl font-bold font-major-mono' to="/">
               Allen Jamison
             </NavLink>
           </div>
-          <div className='sm:w-full md:max-w-sm lg:max-w-md flex justify-between mt-4 sm:mt-0'>
+          <div className='sm:w-full md:max-w-sm lg:max-w-md flex justify-between items-center mt-4 sm:mt-0'>
             <NavLink className='hover:underline decoration-1 w-max' to="/">
               Home
             </NavLink>
@@ -26,7 +26,7 @@ function Footer() {
             </NavLink>
           </div>
         </div>
-        <hr className='my-10 opacity-10' />
+        <hr className='my-6 opacity-10' />
         <div className='flex justify-center items-center text-neutral-400'>
           <Copyright size="16px" className='me-2' /> 2025 Allen Jamison Mendoza
         </div>
