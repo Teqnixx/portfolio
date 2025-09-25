ba2beae8134a60ffc968e95da400ec84addae7be
Allen Jamison <allenjamison027@gmail.com>
2025-09-25 23:44:26 +0800

    Fixed viewport for mobile

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -4,11 +4,11 @@ import BottomNav from "../components/BottomNav"
 
 function MainLayout() {
   return (
-    <div className="min-h-screen flex flex-col sm:flex-row p-2 md:p-4 sm:gap-4 font-poppins bg-background">
+    <div className="min-h-dvh flex flex-col sm:flex-row p-2 pb-20 sm:pb-0 md:p-4 sm:gap-4 font-poppins bg-background">
       <div className="hidden sm:block">
         <SideBar className="md:w-[300px] h-fit p-2 md:p-4 rounded-xl" />
       </div>
-      <main className="flex flex-1 min-h-0 p-4 rounded-xl border border-secondary text-primary mb-16 sm:mb-0">
+      <main className="flex flex-1 min-h-0 p-4 rounded-xl border border-secondary text-primary overflow-y-auto">
         <Outlet />
       </main>
       <div className="sm:hidden">
